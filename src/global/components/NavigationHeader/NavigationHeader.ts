769eb45df1552480a5e2bf45c78791dd6e750cf9
Tomi Heikkala <61519166+hiphurraa@users.noreply.github.com>
2023-07-26 10:13:15 +0300

    Navigation header back arrow replaced with inline SVG.

--- src/global/components/NavigationHeader/NavigationHeader.ts
+++ src/global/components/NavigationHeader/NavigationHeader.ts
@@ -10,7 +10,12 @@ Vue.component('NavigationHeader', {
     
     <header>
         <div class="previous-page" @click="onPrevious">
-             <
+             <svg width="228" height="175" viewBox="0 0 228 175" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <line x1="15" y1="85" x2="228" y2="85" stroke="white" stroke-width="30"/>
+                <line y1="-15" x2="107.589" y2="-15" transform="matrix(-0.715593 0.698517 -0.66943 -0.742875 86.99 0)" stroke="white" stroke-width="30"/>
+                <line y1="-15" x2="107.589" y2="-15" transform="matrix(-0.689586 -0.724204 -0.696134 0.717912 86.1919 174.916)" stroke="white" stroke-width="30"/>
+                <rect x="21.5032" y="64" width="30" height="30" transform="rotate(45.7886 21.5032 64)" fill="white"/>
+            </svg>
         </div>
         <div class="title">{{ title }}</div>    
     </header>
